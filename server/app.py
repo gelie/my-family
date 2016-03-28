8866a3317027e65926d7d3d3fbefaa58aa5e629a
Gavin Elie <gavin.elie@gmail.com>
2016-03-28 20:49:59 +0200

    API 3c - extended sever with more routes

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 """My Family App."""
-from flask import Flask, jsonify
-from flask.ext.restful import Api, Resource, fields, marshal, reqparse
+from flask import Flask, jsonify, request
+from flask.ext.restful import Api, Resource, fields, marshal, reqparse, abort
 from py2neo import Graph, authenticate
 import os
 import time
@@ -25,16 +25,16 @@ person_fields = {
     'name': fields.String,
     'dob': fields.String,
     'sex': fields.String,
-    'uri': fields.Url('person', absolute=True, scheme='https'),
-    'parents': {},
-    'siblings': {}
+    'parents': fields.Url('parents', absolute=True, scheme='https'),
+    'uri': fields.Url('person', absolute=True, scheme='https')
 }
 
 
 class PersonListApi(Resource):
-    """docstring for PersonList."""
+    """Displaying all the persons and adding to the list."""
 
     def __init__(self):
+        """Init method."""
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument('name', type=str, required=True,
                                    help='No name provided',
@@ -48,7 +48,7 @@ class PersonListApi(Resource):
         super(PersonListApi, self).__init__()
 
     def get(self):
-        """."""
+        """Retrieve all Persons."""
         query = """
             match (p:Person)
             return p.name, p.dob, p.sex, id(p)
@@ -58,12 +58,14 @@ class PersonListApi(Resource):
         output = []
         for x in result:
             dob = time.strftime("%c", time.gmtime(x[1]))
-            person = marshal({"name": x[0], "dob": dob, "sex": x[2], "id": x[3]}, person_fields)
+            person = marshal(
+                {"name": x[0], "dob": dob, "sex": x[2],
+                 "id": x[3]}, person_fields)
             output.append(person)
         return {'persons': output}
 
     def post(self):
-        """."""
+        """Create a new Person."""
 
         args = self.reqparse.parse_args()
         person = {
@@ -77,41 +79,73 @@ class PersonListApi(Resource):
             RETURN id(p)
             """
         result = graph.cypher.execute(query, person)
-        id = result[0][0]
-        dob = time.strftime("%c", time.gmtime(person['dob']))
-
-        person['id'] = id
-        person['dob'] = dob
-
-        # result = graph.cypher.execute(query, {"name": args['name'], "dob": args['dob'], "sex": args['sex']})
-        # output = []
-        # for x in result:
-        #     output.append(x.Name)
+        person['id'] = result[0][0]
+        person['dob'] = time.strftime("%c", time.gmtime(person['dob']))
         return {"person": marshal(person, person_fields)}
 
 
 class PersonApi(Resource):
     """docstring for PersonApi."""
 
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('name', type=str, location='json')
+        self.reqparse.add_argument('dob', type=int, location='json')
+        self.reqparse.add_argument('sex', type=str, location='json')
+        super(PersonApi, self).__init__()
+
     def get(self, id):
         """."""
         result = graph.cypher.execute(
             "match(p: Person) where id(p)={id}\
             return p.name, p.dob, p.sex", {"id": id})
-            # "match(p: Person) where p.name='Gavin Lester Elie'\
-            # return p.name, p.dob, p.sex")
         dob = time.strftime("%c", time.gmtime(result[0][1]))
-        person = {"name": result[0][0], "dob": dob, "sex": result[0][2], "id": id}
+        person = {"name": result[0][0], "dob": dob,
+                  "sex": result[0][2], "id": id}
         return {'person': marshal(person, person_fields)}
 
     def put(self, id):
-        """."""
-        pass
+        """Updating a Person's details."""
+        result = graph.cypher.execute(
+            "MATCH (p:Person) WHERE id(p)={id} RETURN p", {"id": id})
+        person = result[0][0].properties
+        args = request.json  # self.reqparse.parse_args()
+        for k, v in args.items():
+            if v is not None:
+                person[k] = v
+
+        return {"person": person}
 
     def delete(self, id):
-        """."""
-        pass
+        """Delete a person and all his relations from the database."""
 
+        query = "MATCH (n) WHERE id(n)={id} DETACH DELETE n"
+        result = graph.cypher.execute(query, {"id": id})
+        return jsonify({"message": "Record %d deleted!" % id})
+
+
+class ParentsListApi(Resource):
+    """Retrieve a person's parents."""
+
+    def get(self, id):
+        """Retrieve a person's parents."""
+        query = """
+            MATCH (a:Person)<-[r:MOTHER_OF|FATHER_OF]-(b)
+            WHERE id(a)={id} 
+            RETURN b.name, b.dob, b.sex, id(b)
+            """
+        result = graph.cypher.execute(query, {"id": id})
+        parents = {}
+        for parent in result:
+            person = {"name": parent[0], "dob": parent[
+                1], "sex": parent[2], "id": parent[3]}
+            person = marshal(person, person_fields)
+            if parent[2] == "F":
+                parents['mother'] = person
+            else:
+                parents['father'] = person
+        return {"parents": parents}
+        # return
 
 api.add_resource(
     PersonListApi,
@@ -122,6 +156,11 @@ api.add_resource(
     PersonApi,
     '/family/api/v1.0/persons/<int:id>',
     endpoint='person')
+
+api.add_resource(
+    ParentsListApi,
+    '/family/api/v1.0/persons/<int:id>/parents',
+    endpoint='parents')
 
 if __name__ == '__main__':
     app.run(debug=True, ssl_context=context)
