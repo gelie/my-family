95e7c8c00a592db7a30bbb4093c3f65afd0c98bb
gelie <gavin.elie@gmail.com>
2016-03-30 17:33:09 +0200

    API 3e - extended parent/sibling features

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
 """My Family App."""
 from flask import Flask, jsonify, request
 from flask.ext.restful import Api, Resource, fields, marshal, reqparse
-from py2neo import Graph, authenticate
+from py2neo import Graph, authenticate, Relationship
+from flask.ext.cors import CORS
 import os
 import time
 import ssl
@@ -11,6 +12,7 @@ context.load_cert_chain('/etc/nginx/ssl/nginx.crt', '/etc/nginx/ssl/nginx.key')
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
 url = os.environ.get('GRAPHENEDB_URL', 'http://localhost:7474')
 username = os.environ.get('NEO4J_USERNAME')
@@ -26,6 +28,7 @@ person_fields = {
     'dob': fields.String,
     'sex': fields.String,
     'parents': fields.Url('parents', absolute=True, scheme='https'),
+    'siblings': fields.Url('siblings', absolute=True, scheme='https'),
     'uri': fields.Url('person', absolute=True, scheme='https')
 }
 
@@ -124,7 +127,7 @@ class PersonApi(Resource):
         return jsonify({"message": "Record %d deleted!" % id})
 
 
-class ParentsListApi(Resource):
+class ParentListApi(Resource):
     """Retrieve a person's parents."""
 
     def __init__(self):
@@ -139,7 +142,7 @@ class ParentsListApi(Resource):
         self.reqparse.add_argument('sex', type=str, required=True,
                                    help='Enter your sex',
                                    location='json')
-        super(ParentsListApi, self).__init__()
+        super(ParentListApi, self).__init__()
 
     def get(self, id):
         """Retrieve a person's parents."""
@@ -156,35 +159,87 @@ class ParentsListApi(Resource):
             person = marshal(person, person_fields)
             if parent[2] == "F":
                 parents['mother'] = person
-            else:
+            elif parent[2] == "M":
                 parents['father'] = person
         return {"parents": parents}
 
     def post(self, id):
-        """Add a person's parent."""
+        """Add a child's parent provided that there aren't 2 already."""
 
-        # check how many parents there are.
-        query = "MATCH (a:Person)<-[r:MOTHER_OF|FATHER_OF]-(b) WHERE id(a)={id} RETURN count(*) as count"
+        childname = graph.cypher.execute("Match (n:Person) where id(n)={id} return n.name", {"id": id})[0][0]
+        query = "MATCH (a:Person)<-[r:MOTHER_OF|FATHER_OF]-(b) WHERE id(a)={id} RETURN count(*)"
         result = graph.cypher.execute(query, {"id": id})
 
-        if (result[0].count < 2):
-            person = {}
+        if len(result) == 0 or (result[0][0] < 2):
             args = self.reqparse.parse_args()
-            for k, v in args.iteritems():
-                if v is not None:
-                    person[k] = v
-            tx = graph.cypher.begin()
-            query = "MERGE (parent:Person {name: {person}.name}) ON CREATE SET parent = {person} RETURN parent"
-            tx.append(query)
-            if person.sex == "M":
+            if args['sex'] == "M":
                 rel = "FATHER_OF"
-            else:
+            elif args['sex'] == "F":
                 rel = "MOTHER_OF"
-            graph.cypher.execute(query, {"person": person})
-            tx.commit()
-            return jsonify({'person': person})
+            child = graph.merge_one("Person", "name", childname)
+            parent = graph.merge_one("Person", "name", args['name'])
+            graph.create_unique(Relationship(parent, rel, child))
+            for k, v in args.iteritems():
+                if k != 'name':
+                    parent[k]=v
+            graph.push(parent)
+            return jsonify({'message': 'Parent has been added successfully.'})
         else:
             return jsonify({'error': 'Person has 2 parents already!!'})
+
+class SiblingListApi(Resource):
+    """docstring for SiblingsApi."""
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('name', type=str, required=True,
+                                   help='No name provided',
+                                   location='json')
+        self.reqparse.add_argument('sex', type=str, required=True,
+                                   help='Enter your sex',
+                                   location='json')
+        super(SiblingListApi, self).__init__()
+    
+    def get(self, id):
+        query = """
+            MATCH (a:Person)-[r:SIBLING_OF]-(b:Person) 
+            WHERE id(a) = {id}
+            RETURN b.name as name, b.dob as dob, b.sex as sex, id(b) as id
+            """
+        result = graph.cypher.execute(query, {"id": id})
+        siblings = []
+        for sibling in result:
+            person = {"name": sibling.name, "dob": sibling.dob,
+                "sex": sibling.sex, "id": sibling.id}
+            person = marshal(person, person_fields) 
+            siblings.append(person)
+            # if sibling.sex == "F":
+            #     siblings['sister'] = person
+            # elif sibling.sex == "M":
+            #     siblings['brother'] = person
+        return {"siblings": siblings}
+
+    def post(self, id):
+        """Add a child's brother/sister."""
+
+        # name = graph.cypher.execute("Match (n:Person) where id(n)={id} return n.name", {"id": id})[0][0]
+        query = "MATCH (a:Person)-[r:SIBLING_OF]-(b:Person) WHERE id(a)={id} RETURN a.name as me, b.name as name"
+        result = graph.cypher.execute(query, {"id": id})
+        args = self.reqparse.parse_args()
+        # if args['sex'] == "M":
+        #     rel = "BROTHER_OF"
+        # elif args['sex'] == "F":
+        #     rel = "SISTER_OF"
+        child = graph.merge_one("Person", "name", result[0].me)
+        sibling = graph.merge_one("Person", "name", args['name'])
+        graph.create_unique(Relationship(sibling, "SIBLING_OF", child))
+        for other_sibling in result:
+            br = graph.merge_one("Person", "name", other_sibling.name)
+            graph.create_unique(Relationship(sibling, "SIBLING_OF", br))
+        for k, v in args.iteritems():
+            if k != 'name':
+                sibling[k]=v
+        graph.push(sibling)
+        return jsonify({'message': 'Sibling has been added successfully.'})
 
 api.add_resource(
     PersonListApi,
@@ -197,9 +252,14 @@ api.add_resource(
     endpoint='person')
 
 api.add_resource(
-    ParentsListApi,
+    ParentListApi,
     '/family/api/v1.0/persons/<int:id>/parents',
     endpoint='parents')
 
+api.add_resource(
+    SiblingListApi,
+    '/family/api/v1.0/persons/<int:id>/siblings',
+    endpoint='siblings')
+
 if __name__ == '__main__':
-    app.run(debug=True, ssl_context=context)
+    app.run(host="0.0.0.0", port=5000, debug=True, ssl_context=context)
